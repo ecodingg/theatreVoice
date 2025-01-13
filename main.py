cdf47b0aaac842e6e0edc19cd45f5c3ce1f6d3ca
eden <ecoding@pm.me>
2025-01-13 15:25:53 -0800

    Project Creation

---
+++ main.py
@@ -0,0 +1,19 @@
+import socket
+import pythonosc
+
+#Steps to develop
+#1 - Open a file and add each line of the file to an array
+#2 - Create a Client
+#3 - Loop through each line. If the word is spoken, GO (/workspace/{id}/go), otherwise wait .01
+
+def main():
+    #Step 1
+
+    #Step 2
+
+    #Step 3
+
+    print("Not done")
+
+
+main()
