ead719021f129c1c031e87db7ea9607073daf23d
eden <ecoding@pm.me>
2025-01-17 14:42:38 -0800

    Added a file for the lines of the show, created a client class, and continued working on Main

---
+++ client.py
@@ -0,0 +1,12 @@
+import socket
+import pythonosc
+
+#Client class
+class Client():
+    #Initialize Class
+    def __init__(self, port, ip):
+        self.port = port
+        self.ip = ip
+
+    #Send Signal
+
--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import socket
 import pythonosc
+import speech_recognition
+
+from client import Client
+
 
 #Steps to develop
 #1 - Open a file and add each line of the file to an array
 #2 - Create a Client
+#2.5 - Create microphone and listener
 #3 - Loop through each line. If the word is spoken, GO (/workspace/{id}/go), otherwise wait .01
 
 def main():
     #Step 1
+    file = open("lines.txt", "r")
+    lines = []
+
+    for line in file:
+        lines.append(line)
 
     #Step 2
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    client = Client(53000,ip)
 
     #Step 3
-
-    print("Not done")
 
 
 main()
