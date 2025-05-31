4b875cf3990d2ff6306a46322ff8c0f426be3816
eden <ecoding@pm.me>
2025-05-31 16:41:58 -0700

    Step 2.5 V1

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #Librarires
 import socket
 import pythonosc
-import speech_recognition
+import speech_recognition as sr
 import pyaudio
 from client import Client
+from test.test_asdl_parser import src_base
 
 #Steps to develop
 #1 - Open a file and add each line of the file to an array
@@ -19,12 +20,22 @@ def main():
     for line in file:
         lines.append(line)
 
+    print("Step 1 done")
     #Step 2
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
     client = Client(53000,ip)
 
+    print("Step 2 done")
     #Step 2.5
+    r = sr.Recognizer()
+    mic = sr.Microphone(device_index=0)
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+    print(r.recognize_amazon(audio))
+
+    print("Step 2.5 done")
 
     #Step 3
 
